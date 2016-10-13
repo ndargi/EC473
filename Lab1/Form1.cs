643efc62e1b842abd1a5e2f864bba36aa0fdde5d
ndargi@bu.edu <NickDargi@Nicks-MacBook-Pro-2.local>
2016-10-13 17:09:07 -0400

    First commit

---
+++ Lab1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void painter(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawString("Nicholas Michael Dargi", Font, Brushes.Black, 100, 100);
+        }
+    }
+}
